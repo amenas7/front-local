1b6eef5ad2c8510396546b825566984428002fef
Alvaro Mena Santos <alvaro.mena@encora.com>
2025-08-06 14:31:13 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SannaUiModule } from 'sanna-ui';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SannaUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
